397cbdbdafcca45d3d64d30f51bfa472c19ac775
Francisco Ortiz <alu0101473055@ull.edu.es>
2025-03-14 19:42:25 +0000

    Clases básicas creadas

--- src/posada.ts
+++ src/posada.ts
@@ -5,9 +5,39 @@ import { Cliente, Raza } from "./clientes";
 
 
 export class Posada {
-  private bienes: Bien[];
-  private mercaderes: Mercader[];
-  private Clientes: Cliente[];
+  private _bienes: Bien[];
+  private _mercaderes: Mercader[];
+  private _clientes: Cliente[];
+  
+  constructor(bienes: Bien[], mercaderes: Mercader[], clientes: Cliente[]) {
+    this._bienes = bienes;
+    this._mercaderes = mercaderes;
+    this._clientes = clientes;
+  }
+  
+  get bienes(): Bien[] {
+    return this._bienes;
+  }
+
+  get mercaderes(): Mercader[] {
+    return this._mercaderes;
+  }
+
+  get clientes(): Cliente[] {
+    return this._clientes;
+  }
+
+  set bienes(bienes: Bien[]) {
+    this._bienes = bienes;
+  }
+
+  set mercaderes(mercaderes: Mercader[]) {
+    this._mercaderes = mercaderes;
+  }
+
+  set clientes(clientes: Cliente[]) {
+    this._clientes = clientes;
+  }
 
 }
 
